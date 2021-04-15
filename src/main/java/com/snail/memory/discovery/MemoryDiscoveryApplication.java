1017ccccdd17ffb3a2499ce3479484817da14f83
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2021-04-15 16:55:45 +0800

    create project

---
+++ src/main/java/com/snail/memory/discovery/MemoryDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.snail.memory.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryDiscoveryApplication.class, args);
+    }
+
+}
